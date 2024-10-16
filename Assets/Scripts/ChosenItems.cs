540518e4a86e8bf03aae6be09ed2ab4b81c75989
melisa1026 <melipan01@gmail.com>
2024-10-16 15:15:10 -0400

    slight readability improvement popup (I think I overcomplicated things oops)

---
+++ Assets/Scripts/ChosenItems.cs
@@ -0,0 +1,8 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChosenItems : MonoBehaviour
+{
+    public static Sprite head, torso, rightArm, leftArm, rightLeg, leftLeg;
+}
--- Assets/Scripts/Popup.cs
+++ Assets/Scripts/Popup.cs
@@ -5,68 +5,63 @@ using DG.Tweening;
 
 public class Popup : MonoBehaviour
 {
-    static int partCount = 0;
-
+    public Vector3 maxPopupSize;
     public GameObject popupObject;
-
-    // these lists have all the options for each body part
-    public Sprite[] torso, head, rightArm, leftArm, rightLeg, leftLeg;
-    
-    // this is the same as the last one, but the sprites have centered pivot points
-    public Sprite[] torsoCentered, headCentered, rightArmCentered, leftArmCentered, rightLegCentered, leftLegCentered;
-
+    static int partCount = 0;
     Sprite popupSprite = null;
 
-    public Vector3 maxPopupSize;
+    // options for each body part      (I need the sprites with a centered pivot for the popup, and sprites with the original pivot for the end scene)
+    public Sprite[] torso, head, rightArm, leftArm, rightLeg, leftLeg;
+    public Sprite[] torsoCentered, headCentered, rightArmCentered, leftArmCentered, rightLegCentered, leftLegCentered;
+
+    List<Sprite[]> torsoArrays, headArrays, rightArmArrays, leftArmArrays, rightLegArrays, leftLegArrays; 
 
     void Start()
     {
         popupObject.SetActive(false);
+
+        torsoArrays = new List<Sprite[]> { torso, torsoCentered };
+        headArrays = new List<Sprite[]> { head, headCentered };
+        rightArmArrays = new List<Sprite[]> { rightArm, rightArmCentered };
+        leftArmArrays = new List<Sprite[]> { leftArm, leftArmCentered };
+        rightLegArrays = new List<Sprite[]> { rightLeg, rightLegCentered };
+        leftLegArrays = new List<Sprite[]> { leftLeg, leftLegCentered };
     }
 
     public void popup()
     {
-        // decide which body part is being picked up
-        Sprite[] optionsCentered = null;
-        Sprite[] optionsNotCentered = null;
 
+        // decide which body part is being picked up
+        List<Sprite[]> options = null;
         switch(partCount) 
         {
             case 0:
-                optionsNotCentered = rightArm;
-                optionsCentered = rightArmCentered;
+                options = rightArmArrays;
                 break;
             case 1:
-                optionsNotCentered = leftLeg;
-                optionsCentered = leftLegCentered;
+                options = leftLegArrays;
                 break;
             case 2:
-                optionsNotCentered = torso;
-                optionsCentered = torsoCentered;
+                options = torsoArrays;
                 break;
             case 3:
-                optionsNotCentered = leftArm;
-                optionsCentered = leftArmCentered;
+                options = leftArmArrays;
                 break;
             case 4:
-                optionsNotCentered = head;
-                optionsCentered = headCentered;
+                options = headArrays;
                 break;
             case 5:
-                optionsNotCentered = rightLeg;
-                optionsCentered = rightLegCentered;
-                break;
-            
+                options = rightLegArrays;
+                break; 
         }
-            
         partCount++;
 
-        if(optionsCentered != null)
+        if(options != null)
         {
-            // choose a random part + assign it to the popup object
-            int numOptions = optionsCentered.Length;
+            // choose a random part 
+            int numOptions = options[1].Length;
             int randomValue = Random.Range(0, numOptions);
-            popupSprite = optionsCentered[randomValue];
+            popupSprite = options[1][randomValue];
             popupObject.GetComponent<SpriteRenderer>().sprite = popupSprite;
 
             // make it pop!
@@ -78,15 +73,20 @@ public class Popup : MonoBehaviour
     public IEnumerator popupIE()
     {
         // make the popup object very small
-            popupObject.SetActive(true);
-            popupObject.GetComponent<Transform>().localScale = new Vector3(0.01f, 0.01f, 1);
+        popupObject.SetActive(true);
+        popupObject.GetComponent<Transform>().localScale = new Vector3(0.01f, 0.01f, 1);
 
-            // make it pop up
-            popupObject.GetComponent<Transform>().DOScale(maxPopupSize*1.05f, 1);
-            popupObject.GetComponent<Transform>().DORotate(new Vector3(0, 0, 720), 1, RotateMode.FastBeyond360);
-            yield return new WaitForSeconds(1);
-            popupObject.GetComponent<Transform>().DOScale(maxPopupSize, 0.2f);
+        // make it pop up
+        popupObject.GetComponent<Transform>().DOScale(maxPopupSize*1.05f, 1);
+        popupObject.GetComponent<Transform>().DORotate(new Vector3(0, 0, 720), 1, RotateMode.FastBeyond360);
+        yield return new WaitForSeconds(1);
+        popupObject.GetComponent<Transform>().DOScale(maxPopupSize, 0.2f);
 
+        // close popup
+        yield return new WaitForSeconds(1);
+        popupObject.GetComponent<Transform>().DOScale(0.01f, 0.2f);
+        yield return new WaitForSeconds(0.2f);
+        popupObject.SetActive(false);
 
     }
 
