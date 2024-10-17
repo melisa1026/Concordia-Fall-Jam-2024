c0bf8839cef62be6761f04205930a991eddcbb6f
melisa1026 <melipan01@gmail.com>
2024-10-17 18:35:52 -0400

    add rain and lightning

--- Assets/Scripts/lightning.cs
+++ Assets/Scripts/lightning.cs
@@ -6,6 +6,8 @@ public class lightning : MonoBehaviour
 {
 
     public GameObject lightBg;
+    public SpriteRenderer[] trees;
+    public Sprite defaultTrees, lightningTrees;
 
     // Start is called before the first frame update
     void Start()
@@ -17,8 +19,16 @@ public class lightning : MonoBehaviour
     IEnumerator lightningClock()
     {
         yield return new WaitForSeconds(6);
+        for(int i=0; i< trees.Length; i++)
+        {   
+            trees[i].sprite = lightningTrees;
+        }
         lightBg.SetActive(true);
         yield return new WaitForSeconds(0.2f);
+        for(int i=0; i< trees.Length; i++)
+        {   
+            trees[i].sprite = defaultTrees;
+        }
         lightBg.SetActive(false);
         StartCoroutine(lightningClock());
     }
