ba01ecaf660768b1d211d0019b7689a8651c5eaa
melisa1026 <melipan01@gmail.com>
2024-10-17 17:11:41 -0400

    add the spotter

--- Assets/Scripts/Spotter.cs
+++ Assets/Scripts/Spotter.cs
@@ -9,15 +9,13 @@ public class Spotter : MonoBehaviour
 {
     public static bool isWatching = false;
     public float waitTime = 5, watchTime = 2, walkInTime = 2;
+    public GameObject inSpot, outSpot;
 
     public void Start()
     {
         // check where the bottom right corner of the screen is
-        Vector3 bottomRightViewportPoint = new Vector3(1, 0, Camera.main.nearClipPlane);
-        Vector3 bottomRightWorldPoint = Camera.main.ViewportToWorldPoint(bottomRightViewportPoint);
 
-        transform.position = new Vector3(bottomRightWorldPoint.x+15, bottomRightWorldPoint.y, transform.position.z);
-
+        transform.position = outSpot.transform.position;
         StartCoroutine(spyTimer());
     }
 
@@ -29,18 +27,15 @@ public class Spotter : MonoBehaviour
 
     public IEnumerator spyComes()
     {
-        // check where the bottom right corner of the screen is
-        Vector3 bottomRightViewportPoint = new Vector3(1, 0, Camera.main.nearClipPlane);
-        Vector3 bottomRightWorldPoint = Camera.main.ViewportToWorldPoint(bottomRightViewportPoint);
 
-        GetComponent<Transform>().DOMove(bottomRightWorldPoint, walkInTime);
+        GetComponent<Transform>().DOMove(inSpot.transform.position, walkInTime);
         yield return new WaitForSeconds(walkInTime);
 
         isWatching = true;
         yield return new WaitForSeconds(watchTime);
         isWatching = false;
         
-        GetComponent<Transform>().DOMove(new Vector3(bottomRightWorldPoint.x+15, bottomRightWorldPoint.y, transform.position.z), walkInTime);
+        GetComponent<Transform>().DOMove(outSpot.transform.position, walkInTime);
         yield return new WaitForSeconds(walkInTime);
         
         StartCoroutine(spyTimer());
